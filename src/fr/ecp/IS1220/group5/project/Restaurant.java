63d69ead4cfc855e73c638dbf74dad08426028a0
Alexandre Carlier <alexandre.carlier01@gmail.com>
2016-11-15 16:01:12 +0100

    Modification Restaurant class

--- src/fr/ecp/IS1220/group5/project/Restaurant.java
+++ src/fr/ecp/IS1220/group5/project/Restaurant.java
@@ -1,5 +1,6 @@
 package fr.ecp.IS1220.group5.project;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class Restaurant extends User {
@@ -7,12 +8,96 @@ public class Restaurant extends User {
 	private Coordinate address;
 	private ArrayList<Item> items = new ArrayList<>();
 	private ArrayList<Meal> meals = new ArrayList<>();
-	
+	private double genericDiscountFactor = 0.05;
+	private double specialDiscountFactor = 0.1;
+
 	public Restaurant(String name, String username, Coordinate address) {
 		super(name, username);
 		this.address = address;
+		retrieveItems();
+		retrieveMeals();
 	}
-	
+
+	public void retrieveItems() {
+		try {
+			FileInputStream fileIn = new FileInputStream("tmp/" + this.id + "items.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+
+			this.items = (ArrayList<Item>) in.readObject();
+
+			in.close();
+			fileIn.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void saveItems(){
+		try {
+			FileOutputStream fileOut = new FileOutputStream("tmp/" + this.id + "items.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+			out.writeObject(this.items);
+
+			out.close();
+			fileOut.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void retrieveMeals() {
+		try {
+			FileInputStream fileIn = new FileInputStream("tmp/" + this.id + "meals.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+
+			this.meals = (ArrayList<Meal>) in.readObject();
+
+			in.close();
+			fileIn.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void saveMeals(){
+		try {
+			FileOutputStream fileOut = new FileOutputStream("tmp/" + this.id + "meals.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+			out.writeObject(meals);
+
+			out.close();
+			fileOut.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public double getGenericDiscountFactor() {
+		return genericDiscountFactor;
+	}
+
+	public void setGenericDiscountFactor(double genericDiscountFactor) {
+		this.genericDiscountFactor = genericDiscountFactor;
+	}
+
+	public double getSpecialDiscountFactor() {
+		return specialDiscountFactor;
+	}
+
+	public void setSpecialDiscountFactor(double specialDiscountFactor) {
+		this.specialDiscountFactor = specialDiscountFactor;
+	}
+
 	public ArrayList<Item> getItems() {
 		return items;
 	}
