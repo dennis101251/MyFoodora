f0346ff92a1a8004cf9f96f19fa630438164105e
DENG Zexi <dengzexi2008@gmail.com>
2016-12-19 12:14:11 +0100

    Choose the type of user

--- src/fr/ecp/IS1220/group5/project/GUI/Register.java
+++ src/fr/ecp/IS1220/group5/project/GUI/Register.java
@@ -5,26 +5,31 @@ import javax.swing.*;
 /**
  * Created by dennis101251 on 2016/12/19.
  */
-public class Register {
+public class Register extends JFrame{
 
-    private int choice;
+//    private int choice;
 //    private JOptionPane jOptionPane;
-    private JFrame jFrame = new JFrame();
+    private JFrame[] jFrame = new JFrame[4];
+    private JLabel name;
 
     public Register(){
-        jFrame = new JFrame("Register");
-//        jOptionPane = new JOptionPane();
-        choice = JOptionPane.showOptionDialog(jFrame,"Message", "Title", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,null,null);
-//        System.out.println(input);
+
+        Object input = JOptionPane.showInputDialog(new JFrame("register"),"Which kind of user you you want to choose?","register", JOptionPane.QUESTION_MESSAGE,null,
+                new String[]{"Customer","Restaurant","Manager","Courier"},"Customer");
+
+        setSize(380, 250);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        name = new JLabel((String) input);
+        add(name);
+
+        setVisible(true);
     }
 
 
-    public int getChoice(){
-        return choice;
-    }
 
     public static void main(String[] args) {
         Register register =  new Register();
-        System.out.println(register.getChoice());
     }
 }
