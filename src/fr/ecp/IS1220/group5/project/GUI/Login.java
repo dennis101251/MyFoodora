fd4488914b5d2c2479e884e154baa75495fc813f
Alexandre Carlier <alexandre.carlier01@gmail.com>
2016-12-20 09:20:20 +0100

    Register button in the login window now closes the login window and opens the register window.

--- src/fr/ecp/IS1220/group5/project/GUI/Login.java
+++ src/fr/ecp/IS1220/group5/project/GUI/Login.java
@@ -140,6 +140,8 @@ public class Login extends JFrame implements ActionListener{
         }
         else if (e.getSource() == registerButton){
             System.out.println("register");
+            Login.this.dispose();
+            Register2 register = new Register2();
         }
     }
 
